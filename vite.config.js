aeeb734295d99d128d8aa1c99be6fb9686a2daaa
Your Name <you@example.com>
2026-05-04 23:38:19 +0200

    add new icon

--- vite.config.js
+++ vite.config.js
@@ -10,16 +10,34 @@ export default defineConfig({
     registerType: 'autoUpdate',
     injectRegister: false,
 
-    pwaAssets: {
-      disabled: false,
-      config: true,
-    },
-
     manifest: {
       name: 'workout-plan',
       short_name: 'workout-plan',
       description: 'workout-plan',
-      theme_color: '#ffffff',
+      theme_color: '#121212',
+      icons: [
+        {
+          src: 'icons/icon_64.png',
+          sizes: '64x64',
+          type: 'image/png'
+        },
+        {
+          src: 'icons/icon_192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: 'icons/icon_512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        },
+        {
+          src: 'icons/icon_512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'maskable'
+        }
+      ]
     },
 
     workbox: {
